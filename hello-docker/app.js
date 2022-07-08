375ae32e3ccdb101d5e3f9f3fa5e82f7aa0bf67a
nuzulfikrie <nuzulfikrie@gmail.com>
2022-07-08 20:43:41 +0800

    add exec function

--- hello-docker/app.js
+++ hello-docker/app.js
@@ -1,1 +1,12 @@
 console.log("Hello docker file changed");
+exec('echo test');
+exec('echo current dir');
+
+exec('pwd');
+exec('echo current dir');
+
+//this function exec
+function exec(cmd, handler = function (error, stdout, stderr) { console.log(stdout); if (error !== null) { console.log(stderr) } }) {
+  const childfork = require('child_process');
+  return childfork.exec(cmd, handler);
+}
